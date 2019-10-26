a587c14e90ca6c8d5d211ca389a4585722ab9d37
李祚 <zuo.li@luckincoffee.com>
2019-10-26 11:19:46 +0800

    创建spring-boot项目

---
+++ src/main/java/zuo/li/play/PlayApplication.java
@@ -0,0 +1,16 @@
+package zuo.li.play;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zuo.li
+ */
+@SpringBootApplication
+public class PlayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayApplication.class, args);
+    }
+
+}
